18e28466d9256144e2a6abcb086a5c9184f86ab0
Jenhan Tao <jenhantao@gmail.com>
2011-10-05 19:19:07 -0700

    added siumlator.pl code

---
+++ simulator.pl
@@ -0,0 +1,10 @@
+#! /usr/bin/perl -w
+$length = @ARGV;
+print "$length arguments entered\n";
+
+#No argument run of this program should give sequence of uniform composition with length of 1 kbp
+if ($length ==0) {
+
+}
+
+
