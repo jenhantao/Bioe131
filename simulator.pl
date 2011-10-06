7d13c5494b42360c3de3fdde2ba813ada82eb4e7
jenhan <jenhan@sol.(none)>
2011-10-06 09:16:27 -0700

    added comments

--- simulator.pl
+++ simulator.pl
@@ -4,7 +4,11 @@ print "$length arguments entered\n";
 
 #No argument run of this program should give sequence of uniform composition with length of 1 kbp
 if ($length ==0) {
-
+	while(<>) {
+	$rand = int(rand($_)); #if range is 100, max number that can be is 99
+	print("$rand\n");	
+	
+	}
 }
 
 
