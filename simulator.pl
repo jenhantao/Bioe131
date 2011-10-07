b5e2a004ca4ef937f7422ef1a5dab3892416f925
jenhan <jenhan@sol.(none)>
2011-10-07 03:13:31 -0700

    finished sequence generator

--- simulator.pl
+++ simulator.pl
@@ -4,12 +4,13 @@ print "$length arguments entered\n";
 
 #No argument run of this program should give sequence of uniform composition with length of 1 kbp
 if ($length ==0) {
-print generateSequence(25,25,25,25,1000,"sequence")."\n";
+print generateSequence(25,25,25,25,100,"sequence")."\n";
 }
 
 
 #This subroutine requires 4 arguments, which should be given in the following order: %A, %T, %C, %G, length, name
 #percentage of A, T, C, G should be given as integers
+#This subroutine returns a string that corresponds to a FASTA 
 sub generateSequence {
 my $length = @_;
 #if block below does basic error checking
@@ -22,11 +23,10 @@ die "must generate a sequence with positive length";
 }
 my $description =">$_[5] $_[0]% A $_[1]% T $_[2]% C $_[3]% G $_[4] bp\n"; #top line gives name, composition, and length
 $length = $_[4] + $_[4] % 3 - 6; #ensure that length is a multiple of 3 and account for start and stop codon
-my $numA = int($_[0]/100*$_[4] * $length);
-my $numT = int($_[1]/100*$_[4] * $length);
+my $numA = int($_[0]/100*$_[4] * $length)-1;
+my $numT = int($_[1]/100*$_[4] * $length)-1;
 my $numC = int($_[2]/100*$_[4] * $length);
-my $numG = int($_[3]/100*$_[4] * $length);
-my $toReturn = "ATG"; #each sequence returned must start with a start codon
+my $numG = int($_[3]/100*$_[4] * $length)-1;
 #randomly select a stop codon and modify the number of A, T, C, and G accordingly
 my $rand = int(rand(3));
 my $stop = "TAG";
@@ -45,8 +45,14 @@ $numT--;
 $numA--;
 $numG--;
 }
-for (my $i =0; $i < $length; $i++) {
+my $toReturn = "ATG"; #each sequence returned must start with a start codon
+for (my $i =1; $i <= $length; $i++) {
 $nucAdded = 0;
+	#every line should only have 80 characters
+	if (length($toReturn)%80 ==0 ) {
+	print(length($toReturn)."\n");
+	$toReturn = $toReturn."\n";
+	}
 	while ($nucAdded == 0){
 	$rand = int(rand(5));
 		if($rand == 0 && $numA > 0) {
@@ -67,9 +73,25 @@ $nucAdded = 0;
 		$nucAdded = 1;
 		}	
 	}
-	if (length($toReturn)%80 ==0) {
-	$toReturn = $toReturn."\n";
+	#this if block replaces the T at the start of a stop codon with another codon to remove a premature stopcodon
+	if (length($toReturn)%3 == 0) {
+		if (substr($toReturn, $i, 3) =~ /(TAG|TAA|TGA)/i) {
+		$rand = int(rand (3));
+			if ($rand == 0) {
+			$numA--;
+			substr($toReturn, $i, 1,"A");
+			} elsif ($rand == 1) {
+			$numC--;
+			substr($toReturn, $i, 1,"C");
+			} elsif ($rand == 2) {
+			$numG--;
+			substr($toReturn, $i, 1,"G");
+			}	
+		}
 	}
+
+	
+
 }
 return $description.$toReturn.$stop;
 }
