ca9a141d01552410481029f5d462212780bd4c86
jenhan <jenhan@sol.(none)>
2011-10-20 00:42:55 -0700

    formatted with subroutine

--- nussinov.pl
+++ nussinov.pl
@@ -5,23 +5,39 @@ die "error: improper number of arguments given\n the only argument should be an 
 } elsif ($ARGV[0] =~ /[^augc]+/) {
 die "error: invalid characters contained in input sequence";
 } 
-
-#for ($i=1; $i<length($ARGV[0]); $i++) {
-$count = 0;
-for ($i=0; $i<4; $i++) {
-@temp = qw(1 2 3 4);
-print "count: $count\n";
-@temp = map{$_+ $count } @temp;
-$count++;
-foreach(@temp) {
+#initialize a 2D array
+for ($i=1; $i<=length($ARGV[0]); $i++) {
+$scores[$i] = ["0"];	
 }
-$arrayOfArrays[$i] = [@temp];	
-}
+calcMaxMatches($ARGV[0]);
 
-foreach my $row(@arrayOfArrays){
+
+#test block
+#for ($i=1; $i<=length($ARGV[0]); $i++) {
+#	for ($j=1; $j<=length($ARGV[0]); $j++) {
+#	$arrayOfArrays[$i][$j] = $i*$j;	
+#	}	
+#}
+foreach my $row(@scores){
    foreach my $val(@$row){
-      print "$val ";
+	if (defined($val)){      
+	print "$val ";
+	}
    }
    print "\n";
 }
 
+sub calcMaxMatches {
+my $seq =$_[0];
+for ($i = length($seq); $i>0; $i--) {
+	$scores[$i][$i]=0;
+	print "position ($i,$i) is 0\n";
+	for ($j=$i+1;$j<=length($seq); $j++) {
+		for ($k=$i+1;$k<=$j-1; $k++) {
+		#assign N(i,j)
+		#@scores[$i][$j] = max ( N(i,j),  N(i,k) + N(k+1,j) 
+		print "i: $i j: $j k: $k\n";
+		}	
+	}
+} 
+}
