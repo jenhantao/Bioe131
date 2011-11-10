ac638b69d9da50d77364718057f27e2896095e7c
jenhan <jenhan@sol.(none)>
2011-11-10 09:21:55 -0800

    worked on input

--- aligner.pl
+++ aligner.pl
@@ -1,4 +1,24 @@
 #! /usr/bin/perl -w
+sub readMatrix {
+if (!-e $_[0]) {
+die "error: specify a valid matrix file";
+} else {
+my @matrix;
+my @letters;
+my @lines;
+open (INPUT, $_[0]);
+	while (<INPUT>) {
+	chomp;
+	push(@lines,$_);
+	}
+close INPUT;
+@letters = split("//",shift(@lines));
+foreach(@lines) {
+print($_."\n");
+}
+}
+}
+
 #This block validates the input argument
 if (@ARGV != 2) {
 die "error: improper number of arguments given\nPlease specify a matrix file and a sequence file only.\n";
@@ -8,19 +28,7 @@ die "error: improper number of arguments given\nPlease specify a matrix file and
 
 #initialize a 2D array
 @scores = ();
-readMatrix(matrix);
+readMatrix ("matrix");
 
 
-sub readMatrix() {
-if (!-e $_[0]) {
-die "error: specify a valid matrix file";
-} else {
-open (INPUT, $_[0]);
-my @loaded;
-	while (<INPUT>) {
-	print $_;
-	}
-close INPUT;
-return @loaded;
-}
-}
+
