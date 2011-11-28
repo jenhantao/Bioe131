010aae31e3b879f4f312c44b8bea9ff5e2ceee05
Jenhan Tao <jenhantao@gmail.com>
2011-11-27 20:37:44 -0800

    added example files for part b

--- hw6b.pl
+++ hw6b.pl
@@ -1,1 +1,21 @@
 #! /usr/bin/perl -w
+
+#this subroutine takes in one argument, which should specify a file containing spike times
+#this subroutine will return an array containing the lines of the input file
+sub readInput {
+open(INPUT, $_[0]);
+my @toReturn = ();
+while(<INPUT>) {
+	chomp;
+	if($_ !~ /^(\s)+/) {
+		push(@toReturn, $_);
+	}
+}
+close(INPUT);
+return @toReturn;
+}
+
+
+sub computeLambda {
+
+}
